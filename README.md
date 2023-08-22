2f87bb5e558e22757667ef77b0bac89ceadfc515
Luca Invernizzi <invernizzi@google.com>
2023-08-22 09:34:27 +0000

    Open source-ifying

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 
 A tool to detect content types with deep learning.
 
-
 ## Setup
 
 ```bash
@@ -10,9 +9,22 @@ $ poetry shell
 $ poetry install
 ```
 
-
 ## Usage
 
 Help: `magika --help`
 
 Example: `magician . -r -d --jsonl`
+
+## Contributing
+
+See [`CONTRIBUTING.md`](CONTRIBUTING.md) for details.
+
+## License
+
+Apache 2.0; see [`LICENSE`](LICENSE) for details.
+
+## Disclaimer
+
+This project is not an official Google project. It is not supported by
+Google and Google specifically disclaims all warranties as to its quality,
+merchantability, or fitness for a particular purpose.
--- tests/test_magika_cli.py
+++ tests/test_magika_cli.py
@@ -1,3 +1,30 @@
+#!/usr/bin/python
+#
+# Copyright 2023 Google, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""TODO: High-level file comment."""
+
+import sys
+
+
+def main(argv):
+    pass
+
+
+if __name__ == '__main__':
+    main(sys.argv)
 import os
 from pathlib import Path
 import subprocess

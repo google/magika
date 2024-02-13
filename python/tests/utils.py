6f7f6c6c39b21b09b5c7411447ae54e4480ad93b
Yanick Fratantonio <yanickf@google.com>
2024-02-13 16:53:42 +0000

    when getting test file paths, only consider files

--- python/tests/utils.py
+++ python/tests/utils.py
@@ -40,7 +40,7 @@ def get_basic_tests_files_dir() -> Path:
 
 def get_basic_test_files_paths() -> List[Path]:
     tests_files_dir = get_basic_tests_files_dir()
-    test_files_paths = sorted(tests_files_dir.iterdir())
+    test_files_paths = sorted(filter(lambda p: p.is_file(), tests_files_dir.iterdir()))
     return test_files_paths
 
 

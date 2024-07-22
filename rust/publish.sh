bb490da8e3feef8a212eb5fa12f288904c4a52c8
Julien Cretin <cretin@google.com>
2024-07-22 13:22:48 +0200

    Fix publish script

--- rust/publish.sh
+++ rust/publish.sh
@@ -16,12 +16,12 @@
 set -e
 . ./color.sh
 
-# This script removes all -dev suffixes and creates a commit.
+# This script removes all -dev version suffixes and creates a commit.
 
 [ -z "$(git status -s)" ] || error "Repository is not clean"
 
 info "Removing all -dev suffixes (if any)"
-sed -i 's/-dev//' $(git ls-files '*/'{Cargo.{toml,lock},CHANGELOG.md})
+sed -i 's/-dev"/"/' $(git ls-files '*/'{Cargo.{toml,lock},CHANGELOG.md})
 if [ -n "$(git status -s)" ]; then
   info "Creating a commit with those changes"
   git commit -aqm'Release Rust crates'

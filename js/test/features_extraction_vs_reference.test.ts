09729c544f7258a07d848a0cdc02ff6465f983c4
Yanick Fratantonio <yanickf@google.com>
2025-04-06 09:23:45 +0000

    js: add copyright

--- js/test/features_extraction_vs_reference.test.ts
+++ js/test/features_extraction_vs_reference.test.ts
@@ -1,7 +1,21 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import { beforeAll, describe, expect, it } from "@jest/globals";
-import { MagikaNode as Magika } from "../magika-node";
-import * as utils from "./utils";
-import { ModelFeatures } from "../src/model-features";
+import { MagikaNode as Magika } from "../magika-node.js";
+import * as utils from "./utils.js";
+import { ModelFeatures } from "../src/model-features.js";
 
 const FEATURES_EXTRACTION_EXAMPLES: FeaturesExtractionExamples = [
   ...parseGzippedFeaturesExtractionExamples(),

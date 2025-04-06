879c681e9ada29af391d2830c749166314522d09
Yanick Fratantonio <yanickf@google.com>
2025-04-06 12:48:42 +0000

    js: add tests for the CLI

---
+++ js/test/magika_cli.test.ts
@@ -0,0 +1,74 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { describe, expect, it } from "@jest/globals";
+import { spawn } from "child_process";
+import path from "path";
+
+describe("index.ts CLI Tests", () => {
+  const scriptPath = path.resolve(__dirname, "../dist/mjs/index.js"); // Adjust path as needed
+  const nodeExecutable = process.execPath; // Path to your Node.js executable
+
+  async function executeCli(
+    args: string[] = [],
+  ): Promise<{ stdout: string; stderr: string; exitCode: number | null }> {
+    return new Promise((resolve, reject) => {
+      const process = spawn(nodeExecutable, [scriptPath, ...args]);
+      let stdout = "";
+      let stderr = "";
+
+      process.stdout.on("data", (data) => {
+        stdout += data.toString();
+      });
+
+      process.stderr.on("data", (data) => {
+        stderr += data.toString();
+      });
+
+      process.on("close", (code) => {
+        resolve({ stdout, stderr, exitCode: code });
+      });
+
+      process.on("error", (err) => {
+        reject(err);
+      });
+    });
+  }
+
+  it("should display help information when no arguments are provided", async () => {
+    const { stdout, stderr, exitCode } = await executeCli();
+    expect(exitCode).toBe(1);
+    expect(stdout).toContain("Usage: ");
+    expect(stdout).toContain("Options:");
+    expect(stderr).toContain("error: missing required argument");
+  });
+
+  it("should process (by path) a specific file and output the expected result", async () => {
+    const filePath = "../README.md";
+    const expectedLabel = "markdown";
+    const { stdout, exitCode } = await executeCli([filePath]);
+    expect(exitCode).toBe(0); // Expect success
+    expect(stdout).toContain(filePath);
+    expect(stdout).toContain(expectedLabel);
+  });
+
+  it("should process (by stream) a specific file and output the expected result", async () => {
+    const filePath = "../README.md";
+    const expectedLabel = "markdown";
+    const { stdout, exitCode } = await executeCli(["--by-stream", filePath]);
+    expect(exitCode).toBe(0); // Expect success
+    expect(stdout).toContain(filePath);
+    expect(stdout).toContain(expectedLabel);
+  });
+});

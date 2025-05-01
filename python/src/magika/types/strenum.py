678c82d72fb4d72f034d679a47714750d69275e6
Yanick Fratantonio <yanickf@google.com>
2025-05-01 22:11:37 +0000

    python: update enum.StrEnum() mapping to string
    This change makes sure our custom enum.StrEnum is compatible with the
    recent enum.StrEnum from Python 3.11's standard library.
    
    Fixes #1054.

--- python/src/magika/types/strenum.py
+++ python/src/magika/types/strenum.py
@@ -16,19 +16,8 @@ from typing import Union
 class StrEnum(str, enum.Enum):
     """
     StrEnum is a Python ``enum.Enum`` that inherits from ``str``. The default
-    ``auto()`` behavior uses the member name as its value.
-
-    Example usage::
-
-        class Example(StrEnum):
-            UPPER_CASE = auto()
-            lower_case = auto()
-            MixedCase = auto()
-
-
-        assert Example.UPPER_CASE == "UPPER_CASE"
-        assert Example.lower_case == "lower_case"
-        assert Example.MixedCase == "MixedCase"
+    ``auto()`` behavior uses the lower-case version of the name. This is meant
+    to reflect the behavior of `enum.StrEnum`, available from Python 3.11.
     """
 
     def __new__(cls, value: Union[str, StrEnum], *args, **kwargs):  # type: ignore[no-untyped-def]
@@ -47,4 +36,4 @@ class StrEnum(str, enum.Enum):
 
 class LowerCaseStrEnum(StrEnum):
     def _generate_next_value_(name, *_):  # type: ignore[no-untyped-def,override]
-        return name.lower().replace("_", "-")
+        return name.lower()

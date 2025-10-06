994d1bf7895d3100ce623976d78f348264984bb2
Yanick Fratantonio <yanick@fratantonio.me>
2025-10-06 12:26:41 +0000

    website: update astro config

--- website-ng/astro.config.mjs
+++ website-ng/astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 
 import vue from "@astrojs/vue";
+import { models } from "@tensorflow/tfjs-node";
 
 // https://astro.build/config
 export default defineConfig({
@@ -20,29 +21,108 @@ export default defineConfig({
         {
           label: "Introduction",
           items: [
-            // Each item here is one entry in the navigation menu.
             { label: "Overview", slug: "introduction/overview" },
-            { label: "Web Demo", slug: "demo/magika-demo" },
+            { label: "Web Demo", slug: "demo/web-demo" },
           ],
         },
         {
-          label: "Demos (for debugging)",
+          label: "Getting Started",
           items: [
-            // Each item here is one entry in the navigation menu.
-            {
-              label: "Counter Demo",
-              slug: "demo/vuetify-counter-demo",
-            },
-            {
-              label: "TextArea Demo",
-              slug: "demo/vuetify-textarea-demo",
-            },
-            { label: "Magika Demo", slug: "demo/magika-demo" },
+            { label: "Installation", slug: "getting-started/installation" },
+            { label: "Quick Start", slug: "getting-started/quick-start" },
           ],
         },
         {
-          label: "Reference",
-          autogenerate: { directory: "reference" },
+          label: "Core Concepts",
+          items: [
+            {
+              label: "How Magika Works",
+              slug: "core-concepts/how-magika-works",
+            },
+            {
+              label: "Models & Content Types",
+              slug: "core-concepts/models-and-content-types",
+            },
+            {
+              label: "Prediction Modes",
+              slug: "core-concepts/prediction-modes",
+            },
+            {
+              label: "Understanding the Output",
+              slug: "core-concepts/understanding-the-output",
+            },
+          ],
+        },
+        {
+          label: "CLI & Bindings",
+          items: [
+            {
+              label: "Overview",
+              slug: "cli-and-bindings/overview",
+            },
+            {
+              label: "Command Line Interface (CLI)",
+              slug: "introduction/overview",
+            },
+            {
+              label: "Python bindings",
+              slug: "introduction/overview",
+            },
+            {
+              label: "Javascript bindings",
+              slug: "introduction/overview",
+            },
+            {
+              label: "Other bindings",
+              slug: "introduction/overview",
+            },
+          ],
+        },
+        {
+          label: "Contributing",
+          items: [
+            {
+              label: "Known Limitations",
+              slug: "contributing/known-limitations",
+            },
+            {
+              label: "How to Contribute",
+              slug: "contributing/how-to-contribute",
+            },
+            {
+              label: "Reporting Security Vulnerabilities",
+              slug: "contributing/reporting-security-vulnerabilities",
+            },
+            {
+              label: "Creating New Bindings",
+              slug: "contributing/creating-new-bindings",
+            },
+          ],
+        },
+        {
+          label: "Resources",
+          items: [
+            {
+              label: "FAQ",
+              slug: "introduction/overview",
+            },
+            {
+              label: "Research Papers and Citation",
+              slug: "introduction/overview",
+            },
+            {
+              label: "Related Posts",
+              slug: "introduction/overview",
+            },
+            {
+              label: "Changelog",
+              slug: "introduction/overview",
+            },
+            {
+              label: "License & Disclaimer",
+              slug: "introduction/overview",
+            },
+          ],
         },
       ],
     }),

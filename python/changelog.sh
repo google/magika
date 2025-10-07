254cb58c2bb358e3fb28754de238e7c8a98e9eeb
Yanick Fratantonio <yanick@fratantonio.me>
2025-10-07 20:07:45 +0200

    Update changelog script to use dynamic branch name

--- python/changelog.sh
+++ python/changelog.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-CHANGED_FILES=$(git diff --name-only origin/main...HEAD)
+CHANGED_FILES=$(git diff --name-only "origin/${1:-main}"...HEAD)
 
 if echo "$CHANGED_FILES" | grep -qE '^python/.*\.py$'; then
   if ! echo "$CHANGED_FILES" | grep -qE '^python/CHANGELOG\.md$'; then

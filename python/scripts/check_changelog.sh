ef406d29b92c67f8d7201e0af1c832b0ff31ca8c
Yanick Fratantonio <yanick@fratantonio.me>
2025-10-07 18:21:55 +0000

    python: moved and renamed check changelog script

--- python/scripts/check_changelog.sh
+++ python/scripts/check_changelog.sh
@@ -3,6 +3,8 @@ set -euo pipefail
 
 CHANGED_FILES=$(git diff --name-only "origin/${1:-main}"...HEAD)
 
+echo "Checking python/* for undocumented changes..."
+
 if echo "$CHANGED_FILES" | grep -qE '^python/.*\.py$'; then
   if ! echo "$CHANGED_FILES" | grep -qE '^python/CHANGELOG\.md$'; then
     echo "::warning title=Changelog Missing::Some changes in the Python package are not documented in python/CHANGELOG.md"

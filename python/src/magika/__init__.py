3c0d7c52f321d104e8c6924df7c0a155885a31bc
Yanick Fratantonio <yanick@fratantonio.me>
2025-10-08 06:23:14 +0000

    python: disable specific linter warnings for specific files

--- python/src/magika/__init__.py
+++ python/src/magika/__init__.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# ruff: noqa: D104
+
 
 __version__ = "0.6.3-dev"
 
--- python/src/magika/types/strenum.py
+++ python/src/magika/types/strenum.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# ruff: noqa: D100, D101, D102, D103, D107
 
 """
 We use a StrEnum backport instead of relying on the newly introduced StrEnum

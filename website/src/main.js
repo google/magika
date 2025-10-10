2dca420aabd0f4b3fea100fb844d775a27349a5c
Luca Invernizzi <invernizzi.l@gmail.com>
2025-10-10 16:38:26 +0200

    Old website redirects to new website, while keeping the model urls working for backwards compatibility (#1175)

--- website/src/main.js
+++ website/src/main.js
@@ -2,9 +2,6 @@ import { createApp } from "vue";
 
 import App from "./App.vue";
 
-import { registerPlugins } from "@/plugins";
-
 // Create the vue app.
 const app = createApp(App);
-registerPlugins(app);
 app.mount("#app");

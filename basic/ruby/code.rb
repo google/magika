a54a07985d61fd04282ae09440d06494d2b20487
Yosef Benny Widyokarsono <yosefbennywidyo@gmail.com>
2025-11-08 23:27:27 +0700

    Add basic ruby code

---
+++ basic/ruby/code.rb
@@ -0,0 +1,11 @@
+class Greeter
+  def initialize(name = "World")
+    @name = name
+  end
+  def say_hi
+    puts "Hi #{@name}!"
+  end
+  def say_bye
+    puts "Bye #{@name}, come back soon."
+  end
+end
